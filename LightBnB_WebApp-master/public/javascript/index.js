39b3d5b8a4aab8a9beeb97b1d53454be2d31e4fa
AbdelrahmanZaytoun <abdelrahmanzaytounn@gmail.com>
2022-01-23 23:13:08 +0300

    add UI

---
+++ LightBnB_WebApp-master/public/javascript/index.js
@@ -0,0 +1,11 @@
+$(() => {
+  getAllListings().then(function (json) {
+    propertyListings.addProperties(json.properties);
+    views_manager.show("listings");
+    // add the new code here
+    $(".reserve-button").on("click", function () {
+      const idData = $(this).attr("id");
+      console.log(idData);
+    });
+  });
+});
